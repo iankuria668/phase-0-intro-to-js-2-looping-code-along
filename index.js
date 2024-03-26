a4346d8bf75487d0a3d9a65493397c1b464e40d3
iankuria668 <iankuria668@gmail.com>
2024-03-26 11:15:12 +0300

    Assignment  Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+writeCards = function(nameProvided){
+    return nameProvided.map(name => `Thank you, ${name}, for the wonderful surprise gift!`)
+}
+
+countDown = function(num){
+    for (let i = num; i >= 0; i--){
+        console.log(i)
+    }
+}
